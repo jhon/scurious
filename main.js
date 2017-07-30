3acaccabaed29f411f80067dda2724cce770fbb1
Jhon Adams <github@l124rd.com>
2017-07-30 13:22:29 -0700

    Towers -> StructureTower prototype + stats cleanup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('prototype.Structure');
 require('prototype.StructureSpawn');
+require('prototype.StructureTower');
 var roleHarvester = require('role.harvester');
 var roleExternalHarvester = require('role.external_harvester');
 var roleUpgrader = require('role.upgrader');
@@ -21,18 +22,12 @@ const DISPATCH_TABLE = {
 
 module.exports.loop = function () { profiler.wrap(function() {
 
-    // Record Start time for teh stats
-    Memory.cpu_stats = { Start: Game.cpu.getUsed() };
-
-    // Keep track of the spawn for ease. We may also loop through all of these at some later point
-    let spawn = Game.spawns['TracteurSpawn'];
-
-    // Keep a local version of creeps? I don't actually use this it was just in the tutorial
+    // Clean up creep memory for creeps we don't have any more :(
     for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name]._move; // Trim _move
             console.log('delete Memory.creeps["' + name + '"] = ' + JSON.stringify(Memory.creeps[name]));
-            if (Memory.creeps[name].ttl > 1 && Memory.creeps[name].last_pos.roomName != spawn.room.name) {
+            if (Memory.creeps[name].ttl > 1 && !(Memory.creeps[name].last_pos.roomName in Game.rooms)) {
                 Memory.last_external_death = Game.time;
             }
             delete Memory.creeps[name];
@@ -40,41 +35,17 @@ module.exports.loop = function () { profiler.wrap(function() {
         }
     }
 
+    // Record Start time for teh stats
+    Memory.cpu_stats = { Start: Game.cpu.getUsed() };
+
     for (let room in Game.rooms)
     {
         let structures = Game.rooms[room].find(FIND_MY_STRUCTURES);
         _.each(structures, (x) => x.run());
     }
 
-    Memory.cpu_stats.CreepManagers = Game.cpu.getUsed() - Memory.cpu_stats.Start;
-
-    // Look through all the towers in the room
-    let towers = spawn.room.find(FIND_MY_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_TOWER; } });
-    for (let t in towers) {
-        let tower = towers[t];
-        // If we can find a hostile, attack it
-        let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (closestHostile) {
-            tower.attack(closestHostile);
-        }
-        // If we can't, try to repair a structure
-        // TODO: Creeps repair for 100HP/energy
-        //        Towers repair for 80HP/energy within 5 units
-        //          dropping off linearly to 20HP/energy at 20 units (and beyond)
-        //        We probably want to find a heuristic to determine whether to let
-        //          the tower do the repair or if we should dispatch a creep. At the
-        //          optimal it's probably worth it. If no creeps are nearby the low end
-        //          may be worth it.
-        else {
-            let closestDamagedStructure = utils.findClosestDamagedStructure(tower);
-            if (closestDamagedStructure) {
-                tower.repair(closestDamagedStructure);
-            }
-        }
-    }
-
-    Memory.cpu_stats.Towers = Game.cpu.getUsed() - Memory.cpu_stats.CreepManagers;
-
+    Memory.cpu_stats.Structures = Game.cpu.getUsed() - Memory.cpu_stats.Start;
+    
     // RUN ALL THE THINGS!
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
@@ -107,13 +78,12 @@ module.exports.loop = function () { profiler.wrap(function() {
         creep.memory.last_pos = new RoomPosition(creep.pos.x, creep.pos.y, creep.room.name);
     }
 
-    Memory.cpu_stats.Creeps = Game.cpu.getUsed() - Memory.cpu_stats.Towers;
+    Memory.cpu_stats.Creeps = Game.cpu.getUsed() - Memory.cpu_stats.Structures;
 
     screepsplus.collect_stats();
 
     Memory.stats.cpu.Start = Memory.cpu_stats.Start;
-    Memory.stats.cpu.CreepManagers = Memory.cpu_stats.CreepManagers;
-    Memory.stats.cpu.Towers = Memory.cpu_stats.Towers;
+    Memory.stats.cpu.Structures = Memory.cpu_stats.Structures;
     Memory.stats.cpu.Creeps = Memory.cpu_stats.Creeps;
     Memory.stats.cpu.used = Game.cpu.getUsed(); // AT END OF MAIN LOOP
     Memory.stats.cpu.stats = Memory.stats.cpu.used - Memory.stats.cpu.Creeps;
---
+++ prototype.StructureTower.js
@@ -0,0 +1,22 @@
+﻿const utils = require('utils');
+
+StructureTower.prototype.run = function () {
+    let closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+        this.attack(closestHostile);
+    }
+    // If we can't, try to repair a structure
+    // TODO: Creeps repair for 100HP/energy
+    //        Towers repair for 80HP/energy within 5 units
+    //          dropping off linearly to 20HP/energy at 20 units (and beyond)
+    //        We probably want to find a heuristic to determine whether to let
+    //          the tower do the repair or if we should dispatch a creep. At the
+    //          optimal it's probably worth it. If no creeps are nearby the low end
+    //          may be worth it.
+    else {
+        let closestDamagedStructure = utils.findClosestDamagedStructure(this);
+        if (closestDamagedStructure) {
+            this.repair(closestDamagedStructure);
+        }
+    }
+}
