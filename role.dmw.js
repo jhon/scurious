403119476f975aee9c1f877f35740d7cff1a785f
Jhon Adams <github@l124rd.com>
2017-07-30 13:31:35 -0700

    Consolidate Creep Prototype Code

--- role.dmw.js
+++ role.dmw.js
@@ -1,43 +1,5 @@
 ﻿var utils = require('utils');
 
-////
-// Original module structure/function comes from @aalku, this module is more hack than man now 
-////
-
-Creep.prototype.recycle = function () {
-    console.log(`Recycling ${this.memory.role} ${this.name}`);
-    this.memory.role_in_life = this.memory.role;
-    this.memory.role = "dmw";
-}
-
-////
-// Suicidal functions courtesy @warinternal
-////
-// Should we recycle or suicide:
-/**
- * Modifer is optional, pass cost of moving to a spawn for recycle
- * Should be clamped to 0 <= x <= this.cost
- */
-Creep.prototype.getRecycleWorth = function (modifier = 0) {
-    return Math.floor(this.memory.cost * 1 * Math.max(0, this.ticksToLive - modifier) / this.memory.ttl_max);
-}
-
-Creep.prototype.getSuicideWorth = function () {
-    return Math.floor(this.memory.cost * CREEP_CORPSE_RATE * this.ticksToLive / this.memory.ttl_max);
-}
-
-/**
- * Math heavy formula for whether a trip to recycle is worth the trouble.
- *
- * Formula states that ttl must be greater than the latter portion to return
- * the minimum amount of energy after the given number of steps.
- * 
- * https://www.symbolab.com/solver/solve-for-equation-calculator/solve%20for%20t%2C%20c%5Ccdot%5Cleft(%5Cfrac%7Bt-d%7D%7Bm%7D%5Cright)%5Cge%20x
- */
-Creep.prototype.isWorthRecycling = function (minReturn = 10, steps = 0) {
-    return this.memory.ttl_max >= ((minReturn * this.memory.ttl_max) + (steps * this.memory.cost)) / this.memory.cost;
-}
-
 module.exports.run = function (in_creep) {
     // More futureproofing than I really need :)
     var r = _.findKey(in_creep.carry);
