2fe67977a172e1a9f0ff5a1343b6ac21cc65fea1
Jhon Adams <github@l124rd.com>
2017-07-30 22:29:28 -0700

    Soldiers will try to stay out of the way

--- role.soldier.js
+++ role.soldier.js
@@ -9,6 +9,25 @@ module.exports.run = function (creep) {
         let hostiles = creep.room.find(FIND_HOSTILE_CREEPS);
         if (hostiles.length == 0)
         {
+            if (creep.pos.x < 4 || creep.pos.x > 46 || creep.pos.y < 4 || creep.pos.y > 46) {
+                utils.moveCreepTo(creep, new RoomPosition(25, 25, creep.memory.work), '#0000aa');
+            }
+            else {
+                // Try to stay out of the way of the drones
+                let drones = creep.room.find(FIND_MY_CREEPS);
+                _.remove(drones, (d) => d.memory.role == 'soldier' || utils.calcDist(d.pos, creep.pos) > 3);
+                drones = _.map(drones, function (d) { return { pos: d.pos, range: 2 }; });
+                let path = PathFinder.search(creep.pos, drones, {
+                    flee: true,
+                    roomCallback: function (room) {
+                        return new PathFinder.CostMatrix;
+                    }
+                });
+                if (path.path.length != 0 && !path.incomplete)
+                {
+                    creep.moveByPath(path.path);
+                }
+            }
             return;
         }
         let hostile = Game.getObjectById(creep.memory.hostile_id);
