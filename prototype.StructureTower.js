4b6c0e4ae9dee3fb4ce2b91111f116a7921d2ae3
Jhon Adams <github@l124rd.com>
2017-07-31 20:32:55 -0700

    Towers reserve 75% power for defense

--- prototype.StructureTower.js
+++ prototype.StructureTower.js
@@ -5,7 +5,8 @@ StructureTower.prototype.run = function () {
     if (closestHostile) {
         this.attack(closestHostile);
     }
-    // If we can't, try to repair a structure
+    // If we can't, try to repair a structure if we have >75% power
+    //   (so we have power for defense)
     // TODO: Creeps repair for 100HP/energy
     //        Towers repair for 80HP/energy within 5 units
     //          dropping off linearly to 20HP/energy at 20 units (and beyond)
@@ -13,7 +14,7 @@ StructureTower.prototype.run = function () {
     //          the tower do the repair or if we should dispatch a creep. At the
     //          optimal it's probably worth it. If no creeps are nearby the low end
     //          may be worth it.
-    else {
+    else if(this.energy > this.energyCapacity*0.75){
         let closestDamagedStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => (structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax * 0.9)
         });
