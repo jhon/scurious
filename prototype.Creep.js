b0d414ebd0dcb33ba8099e99e889a533113ab670
Jhon Adams <github@l124rd.com>
2017-08-01 23:08:14 -0700

    Simplify recycle/suicide code

--- prototype.Creep.js
+++ prototype.Creep.js
@@ -1,37 +1,28 @@
-﻿Creep.prototype.recycle = function () {
+﻿Creep.prototype.euthanize = function () {
+    if (this.memory.work in Memory.exterior_rooms) {
+        _.remove(Memory.exterior_rooms[this.memory.work].creeps[this.memory.role],
+            x => x == this.name);
+    }
+    console.log(`Euthanizing ${this.memory.role} ${this.name}`);
+    this.memory.role_in_life = this.memory.role;
+    this.memory.role = 'euthanized';
+    this.suicide();
+}
+
+Creep.prototype.recycle = function () {
+    if (this.memory.work in Memory.exterior_rooms) {
+        _.remove(Memory.exterior_rooms[this.memory.work].creeps[this.memory.role],
+            x => x == this.name);
+    }
     console.log(`Recycling ${this.memory.role} ${this.name}`);
     this.memory.role_in_life = this.memory.role;
     this.memory.role = "dmw";
 }
 
-////
-// Suicidal functions courtesy @warinternal
-////
-// Should we recycle or suicide:
-/**
- * Modifer is optional, pass cost of moving to a spawn for recycle
- * Should be clamped to 0 <= x <= this.cost
- */
-Creep.prototype.getRecycleWorth = function (modifier = 0) {
-    return Math.floor(this.memory.cost * 1 * Math.max(0, this.ticksToLive - modifier) / this.memory.ttl_max);
+// Determine if walking steps steps and then recycling will net you >= minReturn energy
+Creep.prototype.isWorthRecycling = function (minReturn = 10, steps = 25) {
+    return minReturn >= (((this.memory.ttl - steps) / this.memory.ttl_max) * this.memory.cost);
 }
-
-Creep.prototype.getSuicideWorth = function () {
-    return Math.floor(this.memory.cost * CREEP_CORPSE_RATE * this.ticksToLive / this.memory.ttl_max);
-}
-
-/**
- * Math heavy formula for whether a trip to recycle is worth the trouble.
- *
- * Formula states that ttl must be greater than the latter portion to return
- * the minimum amount of energy after the given number of steps.
- * 
- * https://www.symbolab.com/solver/solve-for-equation-calculator/solve%20for%20t%2C%20c%5Ccdot%5Cleft(%5Cfrac%7Bt-d%7D%7Bm%7D%5Cright)%5Cge%20x
- */
-Creep.prototype.isWorthRecycling = function (minReturn = 10, steps = 0) {
-    return this.memory.ttl_max >= ((minReturn * this.memory.ttl_max) + (steps * this.memory.cost)) / this.memory.cost;
-}
-
 
 ////
 // Creep handler code :)
@@ -51,24 +42,12 @@ Creep.prototype.run = function ()
         // If the creep can't work or move or if it isn't worth keeping around: kill it off
         let parts = _.countBy(_.filter(this.body, (x) => x.hits != 0), "type");
         let part_max = _.countBy(this.body, 'type');
-        let force_suicide = ((!parts.work && part_max.work) ||
+        if ((!parts.work && part_max.work) ||
             (!parts.carry && part_max.carry) ||
             (!parts.claim && part_max.claim) ||
-            !parts.move);
-        if (force_suicide || this.memory.ttl < 0) {
-            if (this.memory.work in Memory.exterior_rooms) {
-                _.remove(Memory.exterior_rooms[this.memory.work].creeps[this.memory.role],
-                    x => x == this.name);
-            }
-            if (force_suicide || !this.isWorthRecycling(10, 25)) {
-                console.log(`Euthanizing ${this.memory.role} ${this.name}`);
-                this.memory.role_in_life = this.memory.role;
-                this.memory.role = 'euthanized';
-                this.suicide();
-            }
-            else {
-                this.recycle();
-            }
+            !parts.move)
+        {
+            this.euthanize();
         }
     }
 
